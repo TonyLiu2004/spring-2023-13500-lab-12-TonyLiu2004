011aa443a73cae2149c57f01cfcb391a88f95bd0
TonyLiu <ltsplice@gmail.com>
2023-04-25 17:18:07 -0400

    fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -11,28 +11,29 @@ int main(){
     std::cout << "-------------------------" << "\n";
 
     std::cout << "TASK B: " << "\n";
-    std::vector<int> a = {-1,-2,3,4,-5,-6,7};
-    std::vector<int> b = goodVibes(a);
-    for(int i =0;i < b.size();i++){
-        cout << b[i] << "\n";
+    std::vector<int> a2 = {-1,-2,3,4,-5,-6,7};
+    std::vector<int> b2 = goodVibes(a2);
+    for(int i =0;i < b2.size();i++){
+        cout << b2[i] << "\n";
     }
     std::cout << "-------------------------" << "\n";
 
     std::cout << "TASK C: " << "\n";
-    std::vector<int> a = {1,2,3,20};
-    std::vector<int> b = {4,5,6};
-    gogeta(a,b);
-    for(int i = 0;i < a.size();i++){
-        std::cout << a[i] << " ";
+    std::vector<int> a3 = {1,2,3,20};
+    std::vector<int> b3 = {4,5,6};
+    gogeta(a3,b3);
+    for(int i = 0;i < a3.size();i++){
+        std::cout << a3[i] << " ";
     }    
     std::cout << "--------------------------" << "\n";
 
     std::cout << "TASK D: " << "\n";
-    std::vector<int> a = {1,2,3,10,11};
-    std::vector<int> b = {3,2,1};
-    std::vector<int> sum = sumPairWise(a,b);
+    std::vector<int> a4 = {1,2,3,10,11};
+    std::vector<int> b4 = {3,2,1};
+    std::vector<int> sum = sumPairWise(a4,b4);
     for(int i = 0;i < sum.size();i++){
         cout << sum[i] << "\n";
     }
+
     return 0;
 }
